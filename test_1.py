a202f2b265395a53186741e9a8c3a08ba759e7d7
user <ugiss34@gmail.com>
2018-10-01 18:00:56 +0300

    20181001_17_46

---
+++ test_1.py
@@ -0,0 +1,15 @@
+#!/user/bin/python
+
+a = 10
+b = 1.55
+c = "A"
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+a = 100000
+b = 1.5567
+c = "BA"
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
