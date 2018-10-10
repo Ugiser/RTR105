cfa81fc50912b64636354d873a0dc3ec0ad242f6
Ugiser <43142504+Ugiser@users.noreply.github.com>
2018-10-10 13:32:57 +0300

    Create dgr_20181010.py

---
+++ dgr_20181010.py
@@ -0,0 +1,179 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(123)
+123
+>>> print('qwe')
+qwe
+>>> print(12.3)
+12.3
+>>> x = 12
+>>> print(x)
+12
+>>> x = 2
+>>> y = 4
+>>> y = 5
+>>> print(y)
+5
+>>> qwe=d
+
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    qwe=d
+NameError: name 'd' is not defined
+>>> qwe = 5
+>>> print(qwe)
+5
+>>> a=1
+>>> b=2
+>>> c=a+b
+>>> print(c)
+3
+>>> x=2
+>>> x=x+2
+>>> print(x)
+4
+>>> print(x)
+4
+>>> x=2*x
+>>> x=2
+>>> print(x)
+2
+>>> x=2*x
+>>> print(x)
+4
+>>> y=56
+>>> y=1+y
+>>> print(1+y)
+58
+>>> z=6
+>>> c=z/4
+>>> print(c)
+1
+>>> k=45
+>>> j=k % 6
+>>> print(j)
+3
+>>> print(2**2)
+4
+>>> print(5**3)
+125
+>>> y=1*2/3**4-5
+>>> print(y)
+-5
+>>> jo = 'q'+ 'r'
+>>> print(jo)
+qr
+>>> type(jo)
+<type 'str'>
+>>> type(c)
+<type 'int'>
+>>> sd= 34.2
+>>> type(sd)
+<type 'float'>
+>>> i=30
+>>> type(i)
+<type 'int'>
+>>> f=float(i)
+>>> print(f)
+30.0
+>>> print(20 % 4)
+0
+>>> f=float(i)
+>>> print(20.0/4.0)
+5.0
+>>> print(20/4)
+5
+>>> n=123
+>>> type(n)
+<type 'int'>
+>>> a=qwe
+>>> qwe=int(q)
+
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    qwe=int(q)
+NameError: name 'q' is not defined
+>>> w=2
+>>> 1
+1
+>>> e='12'
+>>> r=int(e)
+>>> r
+12
+>>> 
+>>> type(e)
+<type 'str'>
+>>> type(r)
+<type 'int'>
+>>> y=input('aka')
+aka
+
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    y=input('aka')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> print('ē', y)
+('\xc4\x93', -5)
+>>> 
+>>> ty=input('asd')
+asdty=input('asd')								jk=int(ty)+1
+
+Traceback (most recent call last):
+  File "<pyshell#68>", line 1, in <module>
+    ty=input('asd')
+  File "<string>", line 1
+    ty=input('asd')								jk=int(ty)+1
+      ^
+SyntaxError: invalid syntax
+>>> i=input('wer')
+wer
+
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    i=input('wer')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> uo=int(i) +1
+>>> print("
+      
+SyntaxError: EOL while scanning string literal
+>>> print('ŗt',uo)
+('\xc5\x97t', 31)
+>>> #123
+>>> 
+>>> 
+>>> 
+>>> 
+>>> m=input(tyu)
+
+Traceback (most recent call last):
+  File "<pyshell#80>", line 1, in <module>
+    m=input(tyu)
+NameError: name 'tyu' is not defined
+>>> h=input("df")
+df fgd
+
+Traceback (most recent call last):
+  File "<pyshell#81>", line 1, in <module>
+    h=input("df")
+  File "<string>", line 1, in <module>
+NameError: name 'fgd' is not defined
+>>> y=input("tyu")
+tyu 56
+>>> type(tyu)
+
+Traceback (most recent call last):
+  File "<pyshell#83>", line 1, in <module>
+    type(tyu)
+NameError: name 'tyu' is not defined
+>>> type(56)
+<type 'int'>
+>>> 
+KeyboardInterrupt
+>>> 
